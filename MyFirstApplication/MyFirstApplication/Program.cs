70240fb92e2ed87d18184e2066b639ad0a8e1ce9
erhan.huz <erhan.huz@EHUZ>
2023-01-12 08:56:27 +0300

    Add project files.

---
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+// duruma bağlı status code bu şekilde gönderilecek
+
+app.Run(async (HttpContext context) =>
+{
+    //Request header'ını okumak için yöntem
+    context.Response.Headers["Content-Type"] = "text/html";    
+        if (context.Request.Headers.ContainsKey("User-Agent"))
+        {
+            string userAgent = context.Request.Headers["User-Agent"];   
+            await context.Response.WriteAsync($"<p>{userAgent}</p>");
+        }
+    
+
+});
+
+app.Run();
