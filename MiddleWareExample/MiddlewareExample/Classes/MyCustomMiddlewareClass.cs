81f78c9c185fb4243696ae972f17d2ba2a9622e1
Erhan HUZ <huzerhan@gmail.com>
2023-01-15 21:12:36 +0300

    end of this topic

--- MiddleWareExample/MiddlewareExample/Classes/MyCustomMiddlewareClass.cs
+++ MiddleWareExample/MiddlewareExample/Classes/MyCustomMiddlewareClass.cs
@@ -1,6 +1,24 @@
-﻿namespace MiddlewareExample.Classes
+﻿using System.Runtime.CompilerServices;
+
+namespace MiddlewareExample.Classes
 {
-    public class MyCustomMiddlewareClass
+    public class MyCustomMiddlewareClass : IMiddleware
     {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("\n\nMy custom mw - starts");
+            await next(context);
+            await context.Response.WriteAsync("\n\nMy custom mw - ends");
+
+        }
+    }
+    // middleware' tek ve basit bir şekilde çağırabilmek için kullanılan extension method 
+    public  static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app) //middleware classında extension metotun başına use kullanmak sadece bir conventiondur. mecbur değiliz
+        {
+           return app.UseMiddleware<MyCustomMiddlewareClass>();
+        }
+            
     }
 }
--- MiddleWareExample/MiddlewareExample/Program.cs
+++ MiddleWareExample/MiddlewareExample/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using MiddlewareExample.Classes;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddlewareClass>(); //diğer mw'yi çağırdıktan sonra sıraya eklemek için bunu koda ekledik
 var app = builder.Build();
 //Middleware'ler çalışmalarının istendiği sıraya göre aşağıda dizilir.
 
@@ -17,32 +20,63 @@ var app = builder.Build();
 
 // ************** aşağıdaki örnekte bu sorunun çözümü işlenecek ***********************
 
-//middleware 1
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("hello");
-    await next(context);
-    await context.Response.WriteAsync("\n\nmw1'e geri dondum"); // buradaki gibi yazılmış mw'lerde response aynı yolu geri izler ve işleme ilk başladığı yerden çıkar
+#region 
 
-});
-
-////istendiği takdirde bu araya da bir middleware eklenebilir. ve bu mw yine Use() metodu ile çağrılmalıdır.
+////middleware 1
 //app.Use(async (HttpContext context, RequestDelegate next) =>
 //{
-//    await context.Response.WriteAsync("\n\nhey there boiiiiii");
+//    await context.Response.WriteAsync("hello");
 //    await next(context);
+//    await context.Response.WriteAsync("\n\nmw1'e geri dondum"); // buradaki gibi yazılmış mw'lerde response aynı yolu geri izler ve işleme ilk başladığı yerden çıkar
+
 //});
 
-//middleware 2
-//run metotuyla kullanılan mw her zaman response contex'inin çıkış noktası olacaktır.
-//ancak run metotu kullanılmadığı takdirde bir problem olmaz çünkü Use metotları  kendi içlerinde response gönderebilir.
-app.Run(async (HttpContext context) =>
-{
-    await context.Response.WriteAsync("\n\nhello again");
-});
-//mw'lerin hepsi peşpeşe geldiğinde pipeline'ı oluşturacaktır. buna aynı zamanda mw chain de denir
+//////istendiği takdirde bu araya da bir middleware eklenebilir. ve bu mw yine Use() metodu ile çağrılmalıdır.
+////app.Use(async (HttpContext context, RequestDelegate next) =>
+////{
+////    await context.Response.WriteAsync("\n\nhey there boiiiiii");
+////    await next(context);
+////});
 
-//custom mw class
+////middleware 2
+////run metotuyla kullanılan mw her zaman response contex'inin çıkış noktası olacaktır.
+////ancak run metotu kullanılmadığı takdirde bir problem olmaz çünkü Use metotları  kendi içlerinde response gönderebilir.
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("\n\nhello again");
+//});
+////mw'lerin hepsi peşpeşe geldiğinde pipeline'ı oluşturacaktır. buna aynı zamanda mw chain de denir
+
+#endregion 
+
+
+#region custom mw class
+
+// middlewareleri kendilerine ait classlar ya da ayrı dosyalarda tutmak kod okunabilirliği için çok daha faydalı olacaktır.
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("\n\n mw 1");
+    await next(context);
+
+
+});
+
+//app.UseMiddleware<MyCustomMiddlewareClass>(); // burada yazılan kod gibi bir satırla ne kadar da kolay mw çalıştırılabiliyor.
+//app.UseMyCustomMiddleware(); //yukarıdaki satıra alternatif olarak MyCustomMiddleware class'ı içinde extension class oluşturduk. bu sayede middleware sınıfını daha kolay şekilde kullanabileceğimiz bir extension method çağırdık. ve yukarıdaki methodun aynısını nu metotdun içinde kullandık.
+
+// .net içerisinde template olarak oluşturulmuş mw classı ile örnek
+app.UseHelloCustomMiddleware();
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("\n\nhey there boiiiiii");
+    await next(context);
+});
+
+#endregion custom mw class
+
+
 
 
 
