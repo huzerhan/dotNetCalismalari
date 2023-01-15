c106861007a31e89b64476b1e69f9d3b8288ad56
Erhan HUZ <huzerhan@gmail.com>
2023-01-15 21:37:51 +0300

    Use when example. UseWhen method lets us create our pipeline depending on the conditions we write.

---
+++ UseWhenExample/UseWhenExample/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+//UseWhen metotu iki parametre alır.
+//ilk parametre, boolean değer döndürecek bir lambda ifadesidir
+//ikinci parametre ise şart true döndüğünde işlenecek koddur
+app.UseWhen(
+    context => context.Request.Query.ContainsKey("username"),
+//context=>true,
+    app => {
+        app.Use(async (context,next) =>
+        {
+            await context.Response.WriteAsync("\n\nhello from mw branch");
+            await next();
+        });
+    });
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("\n\nhello from mw at main chain");
+});
+app.Run();
